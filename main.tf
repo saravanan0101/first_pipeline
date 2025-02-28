b60b0dd77659b7a07d685d1392d943a4547a92bc
Saravanan R <rajamanisaravanan.rs@gmail.com>
2025-02-28 11:55:25 +0530

    pipe_13

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "vm" {
                      
 }
 
-resource "aws_instance" "vm" {
+resource "aws_instance" "vm1" {
     ami = "ami-04b4f1a9cf54c11d0"
     instance_type = "t2.micro"
    tags = {
